8071f3bde8868f44e43e807511208e160c1a19b6
PikoLab <nonie.115@gmail.com>
2021-07-17 12:02:09 +0800

    aws lambda

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Rank=({name,entries})=>{
-    return(
-        <div className='f2' >
-            <div>
-                {`${name}, your current entry count is...`}
+class Rank extends Component{
+    constructor(){
+        super();
+        this.state={
+            emoji:''
+        }
+    }
+
+    componentDidMount(){
+        this.generateEmoji(this.props.entries)
+    }
+
+    componentDidUpdate(prevProps,prevState){
+        if (prevProps.entries === this.props.entires && prevProps.name===this.props.name){
+            return null
+        }
+        this.generateEmoji(this.props.entries);
+    }
+
+    generateEmoji = (entries) => {
+        fetch(`https://YOUR-ENDPOINT.amazonaws.com/prod/rank?rank=${entries}`)  //YOUR AWS LAMBDA ENDPOINT
+        .then(response => response.json())
+        .then(data => this.setState({emoji: data.input}))
+        .catch(console.log)
+    }
+
+    render(){
+        return(
+            <div className='f2' >
+                <div>
+                    {`${this.props.name}, your current entry count is...`}
+                </div>
+                <div className='f1'>
+                    {this.props.entries}
+                </div>
+                <div className='f3'>
+                    {`Rank Badge: ${this.state.emoji}`}
+                </div>
             </div>
-            <div className='f1'>
-                {entries}
-            </div>
-        </div>
-    );
+        );
+
+    }
 }
 
 export default Rank; 
